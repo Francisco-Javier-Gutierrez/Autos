99b3b7353037961ea5d3f4435b5683e81b1a6cdf
Francisco Javier Gutierrez Herculano <chinopaco.05@gmail.com>
2026-06-16 12:35:48 -0600

    Inicialización del proyecto

---
+++ src/main/java/com/utsem/app/ProyectoAutosApplication.java
@@ -0,0 +1,13 @@
+package com.utsem.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAutosApplication.class, args);
+	}
+
+}
